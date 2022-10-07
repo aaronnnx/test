7f433ad81ae29de29b74427f45db4cf21d033570
Aaron Tung Xingron <74345856+aaronnnx@users.noreply.github.com>
2022-10-07 08:12:01 +0800

    Staff's Homepage

---
+++ staffHomepage.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+  <html lang="en">
+    <style>
+      body 
+      {
+        background-color: #F1E9E8;
+        font-family:Montserrat;
+        font-size:11px;
+      }
+
+      ul.horizontal 
+      {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #4A4848;
+      }
+
+      ul.horizontal li  
+      {
+        display: inline-block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+      }
+
+    </style>
+	
+<body>
+   
+	<ul class="horizontal">
+    <li>Add Member</li>
+		<li>Add Sales Record</li>
+		<li>Add Grocery Item</li>
+		<li>Edit Personal Details</li>
+	</ul>
+
+</body>
+</html>
